dab67acc1624657a3d44821710df76bbdc1e8667
SAI XTUN <htunminlatt.iosdev@gmail.com>
2025-02-19 19:08:32 +0700

    //update noti

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,31 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
+override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+) -> Bool {
+ FirebaseApp.configure()
+ Messaging.messaging().delegate = self
+ GeneratedPluginRegistrant.register(with: self)
+
+  if #available(iOS 10.0, *) {
+    // For iOS 10 display notification (sent via APNS)
+    UNUserNotificationCenter.current().delegate = self
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+} else {
+    let settings: UIUserNotificationSettings =
+    UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+    application.registerUserNotificationSettings(settings)
 }
+application.registerForRemoteNotifications()
+return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+   }
